ef10d39833a50a6b944d0f67e18f79cb478b775c
Neil McHenry <neilmchenry@gmail.com>
2019-04-10 23:50:59 -0500

    Added Trigger Spheres

---
+++ Source/CubeSatAssembly/triggerSat.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerSphere.h"
+#include "triggerSat.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBESATASSEMBLY_API AtriggerSat : public ATriggerSphere
+{
+	GENERATED_BODY()
+	
+};
